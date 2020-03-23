b737523c0b8cb50d91c139fd7f2aa4840d0f798a
Anshuman634 <arnav.tyagi2001@gmail.com>
2020-03-23 21:24:51 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,56 @@
+
+
+
+function setup()
+{
+  createCanvas(800,500);
+}
+
+function draw()
+{
+  background("sky blue");
+  fill("purple");
+  rect(144,230,136,270);
+  rect(180,202,28,28);
+  rect(215,202,28,28);
+  rect(250,202,28,28);
+  rect(143,202,28,28);
+  fill("purple");
+  rect(278,230,290,270);
+  fill("purple");
+  rect(568,230,134,270);
+  rect(568,202,28,28);
+  rect(603,202,28,28);
+  rect(638,202,28,28);
+  rect(673,202,28,28);
+  fill("brown");
+  rect(327,280,95,220);
+  rect(422,280,95,220)
+  circle(405,400,20);
+  circle(449,400,20);
+  
+  fill("blue");
+  rect(277,200,290,30);
+  fill("green");
+  rect(0,480,800,20);
+  rectMode(CENTER);
+  fill("yellow");
+  circle(70,120,60,60);
+  rect(70,160,3,20);
+  rect(110,119,20,3);
+  rect(70,80,3,20);
+  rect(30,119,20,3);
+  fill("brown");
+  rect(421,190,100,20);
+  rect(421,170,80,20);
+  rect(421,150,60,20);
+  rect(421,96,7,90);
+  fill("orange");
+  rect(465,61,80,20);
+  fill("white");
+  rect(465,81,80,20);
+  fill("green");
+  rect(465,101,80,20);
+  fill("blue");
+  ellipse(465,80,20,20);               
+}
